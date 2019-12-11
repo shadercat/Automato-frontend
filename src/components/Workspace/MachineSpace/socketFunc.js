15ff22fb3e538fbb174558f2ba26c7dcf6774262
Thang <shadercatin@gmail.com>
2019-12-12 01:14:57 +0200

    - delete unused functions

--- src/components/Workspace/MachineSpace/socketFunc.js
+++
@@ -1,9 +0,0 @@
-import openSocket from 'socket.io-client';
-
-const socket = openSocket('http://localhost:3002');
-const subscribeToMachines = function (id, cb) {
-    socket.on('machinesChange', data => cb(data));
-    socket.emit('machinesChange', id);
-};
-
-export {subscribeToMachines};
