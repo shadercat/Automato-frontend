6f0f2bd1ae2974e74d1dcf27137de30a578a9aba
shadercat <shadercatin@gmail.com>
2020-05-06 12:39:54 +0300

    - change connection string;

--- src/constants/GlobalVal.js
+++ src/constants/GlobalVal.js
@@ -1,4 +1,4 @@
-const Main = "https://automato-backend.herokuapp.com/"; //http://localhost:3000/
+const Main = "https://automato-backend.azurewebsites.net/"; //http://localhost:3000/
 const Paths = {
     userLogin: Main + "users/login",
     userLogout: Main + "users/logout",
